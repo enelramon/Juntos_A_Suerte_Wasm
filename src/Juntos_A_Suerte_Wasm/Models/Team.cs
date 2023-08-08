cb0d2496e3bc61dbb52fb4bd38586e8eba259700
Enel Almonte <enel_almonte@ucne.edu.do>
2023-08-08 18:52:11 -0400

    Adding team service

---
+++ src/Juntos_A_Suerte_Wasm/Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Juntos_A_Suerte_Wasm.Models;
+
+public class Team
+{
+    [Key]
+    public int TeamId { get; set; }
+
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    public string? Name { get; set; }
+
+    public List<Person> Members { get; set; } = new List<Person>();
+}
---
+++ src/Juntos_A_Suerte_Wasm/Services/TeamService.cs
@@ -0,0 +1,44 @@
+﻿using Blazored.LocalStorage;
+using Juntos_A_Suerte_Wasm.Models;
+
+namespace Juntos_A_Suerte_Wasm.Services;
+
+public class TeamService
+{
+    private readonly ILocalStorageService _localStorage;
+    private const string StorageKey = "teams";
+
+    public TeamService(ILocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+    }
+
+    public async Task AddTeamAsync(Team team)
+    {
+        var teams = await GetTeamsAsync();
+        team.TeamId = teams.Any() ? teams.Max(t => t.TeamId) + 1 : 1;
+        teams.Add(team);
+        await SaveTeamsAsync(teams);
+    }
+
+    private async Task SaveTeamsAsync(List<Team> teams)
+    {
+        await _localStorage.SetItemAsync(StorageKey, teams);
+    }
+
+    public async Task<List<Team>> GetTeamsAsync()
+    {
+        return await _localStorage.GetItemAsync<List<Team>>(StorageKey) ?? new List<Team>();
+    }
+
+    public async Task DeleteTeamAsync(int teamId)
+    {
+        var teams = await GetTeamsAsync();
+        var teamToRemove = teams.FirstOrDefault(t => t.TeamId == teamId);
+        if (teamToRemove != null)
+        {
+            teams.Remove(teamToRemove);
+            await SaveTeamsAsync(teams);
+        }
+    }
+}
