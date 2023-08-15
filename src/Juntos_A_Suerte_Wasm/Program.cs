0349fe2ec8a99aaca4940a853370e1868205ad98
Robert Tejada <robert_tejada@ucne.edu.do>
2023-08-15 01:48:03 -0400

    Resolviendo Issue #1, Extensor de Notificaciones

--- src/Juntos_A_Suerte_Wasm/Program.cs
+++ src/Juntos_A_Suerte_Wasm/Program.cs
@@ -3,12 +3,17 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Juntos_A_Suerte_Wasm;
 using Blazored.LocalStorage;
 using Juntos_A_Suerte_Wasm.Services;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<PersonService>(); // Register as a singleton
