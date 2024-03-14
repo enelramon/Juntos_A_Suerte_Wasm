f668299276596ed36588533ca2142f6d68c0b59e
Robert Tejada <robert_tejada@ucne.edu.do>
2024-03-13 20:48:16 -0400

    Add .gitignore for Juntos_A_Suerte_Compose, add app resources and theme files

---
+++ src/Juntos_A_Suerte_Compose/app/src/main/java/com/ucne/juntos_a_suerte_compose/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.ucne.juntos_a_suerte_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ucne.juntos_a_suerte_compose.ui.theme.Juntos_A_Suerte_ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Juntos_A_Suerte_ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Juntos_A_Suerte_ComposeTheme {
+        Greeting("Android")
+    }
+}
