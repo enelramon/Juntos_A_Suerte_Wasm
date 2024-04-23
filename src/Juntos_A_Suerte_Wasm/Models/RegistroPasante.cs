7e0443c28e609f6d4519a4386b58332e4e27eed9
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-04-23 15:14:49 -0400

    Este es el Modelo del Registro del Pasante. Aqui se guardaran los datos que seran destinados a la Carta.

---
+++ src/Juntos_A_Suerte_Wasm/Models/RegistroPasante.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Juntos_A_Suerte_Wasm.Models;
+
+public class RegistroPasante
+{
+	[Key]
+	public int CodigoRegistro {  get; set; }
+
+	[Required(ErrorMessage ="Este campo es Obligatorio")]
+	public string? Nombre { get; set; }
+
+	[Required(ErrorMessage = "Este campo es Obligatorio")]
+	public string? Apellido { get; set; }
+
+	[Required(ErrorMessage = "Este campo es Obligatorio")]
+	public string? Cedula { get; set;}
+
+	[Required(ErrorMessage = "Este campo es Obligatorio")]
+	public string? Matricula { get; set; }
+
+	[Required(ErrorMessage = "Este campo es Obligatorio")]
+	public string? Pasantia { get; set; }
+
+	[Required(ErrorMessage = "Este campo es Obligatorio")]
+	public DateTime? DiaInicio { get; set; } = DateTime.Now;
+
+	[Required(ErrorMessage = "Este campo es Obligatorio")]
+	public DateTime DiaFinal { get; set; } = DateTime.Now;
+
+	[Required(ErrorMessage = "Este campo es Obligatorio")]
+	public int CantidadHoras { get; set; }
+}
