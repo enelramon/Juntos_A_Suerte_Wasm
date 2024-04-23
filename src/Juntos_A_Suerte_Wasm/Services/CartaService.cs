07f158ba46d57e50cfc9c857832642cc378446a0
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-04-23 15:49:20 -0400

    Haciendo el servicio con el LocalStorage

---
+++ src/Juntos_A_Suerte_Wasm/Services/CartaService.cs
@@ -0,0 +1,24 @@
+﻿using Blazored.LocalStorage;
+using Juntos_A_Suerte_Wasm.Models;
+namespace Juntos_A_Suerte_Wasm.Services;
+
+public class CartaService
+{
+	private readonly ILocalStorageService _localStorage;
+	private const string StorageKey = "pasantes";
+
+	public CartaService(ILocalStorageService localStorage)
+	{
+		_localStorage = localStorage;
+	}
+
+	public async Task SaveOrUpdatePasanteAsync(RegistroPasante pasante)
+	{
+		await _localStorage.SetItemAsync(StorageKey, pasante);
+	}
+
+	public async Task<RegistroPasante?> GetPasanteAsync()
+	{
+		return await _localStorage.GetItemAsync<RegistroPasante>(StorageKey);
+	}
+}
