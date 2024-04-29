b377ce108f131e33dcb7020b3337bbcf14666724
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-04-28 20:24:50 -0400

    Agregando las Validaciones

--- src/Juntos_A_Suerte_Wasm/Models/RegistroPasante.cs
+++ src/Juntos_A_Suerte_Wasm/Models/RegistroPasante.cs
@@ -8,19 +8,25 @@ public class RegistroPasante
 	public int CodigoRegistro {  get; set; }
 
 	[Required(ErrorMessage ="Este campo es Obligatorio")]
-	public string? Nombre { get; set; }
+    [RegularExpression(@"^[A-ZÁÉÍÓÚÑ][a-zA-ZÁÉÍÓÚÑ ]*$", ErrorMessage = "El Apellido debe comenzar con una letra mayúscula y no debe contener números.")]
+    public string? Nombre { get; set; }
 
 	[Required(ErrorMessage = "Este campo es Obligatorio")]
-	public string? Apellido { get; set; }
+    [RegularExpression(@"^[A-ZÁÉÍÓÚÑ][a-zA-ZÁÉÍÓÚÑ ]*$", ErrorMessage = "El Apellido debe comenzar con una letra mayúscula y no debe contener números.")]
+    public string? Apellido { get; set; }
 
 	[Required(ErrorMessage = "Este campo es Obligatorio")]
-	public string? Cedula { get; set;}
+    [RegularExpression(@"^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s\W]{1,13}$", ErrorMessage = "La Cédula debe tener máximo 13 caracteres alfanuméricos, incluyendo símbolos")]
+    public string? Cedula { get; set;}
 
     [Required(ErrorMessage = "Este campo es Obligatorio")]
+    [RegularExpression(@"^[A-ZÁÉÍÓÚÑ][a-zA-ZÁÉÍÓÚÑ ]*$", ErrorMessage = "La Carrera debe comenzar con una letra mayúscula y no debe contener números.")]
+
     public string? Carrera { get; set; }
 
     [Required(ErrorMessage = "Este campo es Obligatorio")]
-	public string? Matricula { get; set; }
+    [RegularExpression(@"^\d{8}$", ErrorMessage = "La Matrícula debe contener exactamente 8 dígitos.")]
+    public string? Matricula { get; set; }
 
 	[Required(ErrorMessage = "Este campo es Obligatorio")]
 	public string? Pasantia { get; set; }
