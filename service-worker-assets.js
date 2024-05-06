04ef69727e80f82b6ebcc8b81febabdf50b28805
Ruth2002Casilla <Ruth2002Casilla@users.noreply.github.com>
2024-05-06 18:36:12 +0000

    Deploying to gh-pages from @ enelramon/Juntos_A_Suerte_Wasm@a575ad5408fa83e445e37e4903fef5041fa10389 🚀

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -273,7 +273,7 @@
       "url": "_framework\/icudt.dat"
     },
     {
-      "hash": "sha256-b01bNv7Re2FLbDRr+D4wrXfhAliLaHfCysiyEq6Gu3s=",
+      "hash": "sha256-zLjgTDzBDt\/L3JbzyvyU0P9mBCuHjWDsrVUpMNSB65g=",
       "url": "_framework\/blazor.boot.json"
     },
     {
@@ -289,7 +289,7 @@
       "url": "_framework\/iTextSharp.LGPLv2.Core.dll"
     },
     {
-      "hash": "sha256-UrQgaCOkvM2Z41H9JPj8V1TMzr46l9GBTVxzF+j5qEo=",
+      "hash": "sha256-iGYaY1qtIUfbr4Ny+NMvtvfS6XHEgAp9dWk2U3ohtSc=",
       "url": "_framework\/Juntos_A_Suerte_Wasm.dll"
     },
     {
@@ -949,5 +949,5 @@
       "url": "_content\/Radzen.Blazor\/Radzen.Blazor.js"
     }
   ],
-  "version": "ctQGa8Bg"
+  "version": "B5bQ3iIq"
 };
--- service-worker.js
+++ service-worker.js
@@ -45,4 +45,4 @@ async function onFetch(event) {
 
     return cachedResponse || fetch(event.request);
 }
-/* Manifest version: ctQGa8Bg */
+/* Manifest version: B5bQ3iIq */
